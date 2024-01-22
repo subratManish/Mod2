d316f339206c26def0f92415c31b6cc82586afa8
subratManish <154910483+subratManish@users.noreply.github.com>
2024-01-22 10:41:13 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,15 +4,15 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hrdhat = require("hardhat");
+const hardhat = require("hardhat");
 
 async function startFunction() {
   const initAmount = 1;
-  const RahulWallet = await hrdhat.ethers.getContractFactory("RahulKumarWallet");
-  const RahulAssessment = await RahulWallet.deploy(initAmount);
-  await RahulAssessment.deployed();
+  const SubratWallet = await hardhat.ethers.getContractFactory("SubratKumarWallet");
+  const SubratAssessment = await SubratWallet.deploy(initAmount);
+  await SubratAssessment.deployed();
 
-  console.log(`Contract deployed to ${RahulAssessment.address} with initial balance = ${initAmount} `);
+  console.log(`Contract deployed to ${SubratAssessment.address} with initial balance = ${initAmount} `);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
